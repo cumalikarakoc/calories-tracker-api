22fdfc6c1bf43802ad66ef354c8f2b1e3165d70b
Cumali Karakoc <C.Karakoc@student.han.nl>
2019-07-25 16:47:11 +0200

    Add migrations for ingredientmeal

---
+++ DataContext/Migrations/20190725131419_IngredientMealRelation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataContext.Migrations
+{
+    public partial class IngredientMealRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IngredientMeal",
+                columns: table => new
+                {
+                    IngredientId = table.Column<int>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    MealId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientMeal", x => new { x.IngredientId, x.CreatedAt });
+                    table.ForeignKey(
+                        name: "FK_IngredientMeal_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientMeal_Meals_MealId",
+                        column: x => x.MealId,
+                        principalTable: "Meals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientMeal_MealId",
+                table: "IngredientMeal",
+                column: "MealId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientMeal");
+        }
+    }
+}
