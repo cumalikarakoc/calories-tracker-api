4a0deba5d0b2e1567829d401f015c22be3f69651
MaartenGDev <maarten.grootoonk@gmail.com>
2019-08-08 12:01:09 +0200

    Add stats endpoint for prometheus

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Prometheus;
 
 namespace Web
 {
@@ -71,6 +72,9 @@ namespace Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMetricServer();
+            app.UseHttpMetrics();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
